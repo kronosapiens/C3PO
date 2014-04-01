9c3f9a13a90eae0ae49247bc1b78457bbda4cf28
Daniel Kronovet <kronovet@gmail.com>
2014-04-01 08:10:49 -0400

    initialized js version

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ coffeemeter
 ===========
 
 An arduino-powered fullness-meter for the Flatiron School coffeepot.
+
+
+## Using Artoo:
+
+Analog sensor: http://artoo.io/documentation/drivers/analog-sensor/
+
+Led: http://artoo.io/documentation/drivers/led/
+
+## Using Johnny-Five
+
+https://github.com/rwaldron/johnny-five
+
+https://github.com/rwaldron/johnny-five/blob/master/docs/sensor.md
--- coffeemeter.rb
+++ coffeemeter.rb
@@ -1,12 +1,31 @@
 require 'artoo'
 
-connection :arduino, adaptor: :firmata, port: '127.0.0.1:8023'
-device :led, driver: :led, pin: 13
-device :button, driver: :button, pin: 2, interval: 0.01
+connection :arduino, :adaptor => :firmata, :port => '127.0.0.1:8023'
+# Analog inputs are based from 0 to 5 on the Arduino UNO, may vary on other arduino boards
+device :sensor, driver: :analog_sensor, pin: 0, interval: 0.25, upper: 900, lower: 200
+device :led, :driver => :led, :pin => 8
 
 work do
-  puts "Press the button connected on pin #{ button.pin }..."
+  puts
+  puts "Reading sensor in analog pin #{ sensor.pin }"
+  puts "Reading intervals every => #{ sensor.interval }"
+  puts "Initial sensor value => #{ sensor.analog_read(0) }"
+  puts "Sensor upper trigger set at value => #{ sensor.upper }"
+  puts "Sensor lower trigger set at value => #{ sensor.lower }"
 
-  on button, :push    => proc { led.on }
-  on button, :release => proc { led.off }
+  on sensor, :upper => proc {
+    puts "UPPER LIMIT REACHED!"
+    led.off
+  }
+
+  on sensor, :lower => proc {
+    puts "LOWER SENSOR LIMIT REACHED!"
+    led.on
+  }
 end
+
+# how to call events on your drivers
+on sensor, :upper => proc { puts "UPPER LIMIT REACHED!" }
+on sensor, :lower => proc { puts "LOWER LIMIT REACHED!" }
+
+analog_read(pin_number)
